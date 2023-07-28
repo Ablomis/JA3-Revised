4fcc07f1f134a2b563c4746a2471ccf6fac028ab
Ablomis <ablomis@gmail.com>
2023-07-28 14:40:02 -0700

    SniperDeploy

---
+++ Code/UnitActions.lua
@@ -0,0 +1,38 @@
+  function Unit:SniperTarget(action_id, cost_ap, args)
+    print('Targeting')
+    args.permanent = true
+    --args.num_attacks = self:GetNumMGInterruptAttacks()
+    args.num_attacks=1
+    self.interruptable = false
+    return self:OverwatchAction(action_id, cost_ap, args)
+end
+
+function Unit:SniperSetup(action_id, cost_ap, args)
+    print('SniperSetup')
+    self.interruptable = false
+    if self.stance ~= "Prone" then
+      self:DoChangeStance("Prone")
+    end
+    self:AddStatusEffect("StationedSniper")
+    print('StationedSniper')
+    self:UpdateHidden()
+    self:FlushCombatCache()
+    self:RecalcUIActions(true)
+    ObjModified(self)
+    return self:SniperTarget(action_id, cost_ap, args)
+  end
+
+  function Unit:SniperPack()
+    self:InterruptPreparedAttack()
+    self:RemoveStatusEffect("StationedSniper")
+    self:UpdateHidden()
+    self:FlushCombatCache()
+    self:RecalcUIActions(true)
+    if HasPerk(self, "KillingWind") then
+      self:RemoveStatusEffect("FreeMove")
+      self:AddStatusEffect("FreeMove")
+    end
+    ObjModified(self)
+  end
+
+  
